3378dcc1830234a6c0ea4543aba7afafd0ec137f
BluSpring <bluspringindohq@gmail.com>
2026-03-28 09:56:26 +0800

    Update to 26.1, closes #3

--- src/main/java/xyz/bluspring/nodynamicfps/NoDynamicFps.java
+++ src/main/java/xyz/bluspring/nodynamicfps/NoDynamicFps.java
@@ -1,12 +1,11 @@
 package xyz.bluspring.nodynamicfps;
 
 import com.chocohead.mm.api.ClassTinkerers;
-import net.fabricmc.loader.api.FabricLoader;
 
 public class NoDynamicFps implements Runnable {
     @Override
     public void run() {
-        ClassTinkerers.enumBuilder(FabricLoader.getInstance().getMappingResolver().mapClassName("intermediary", "net.minecraft.class_9927"), String.class, String.class)
+        ClassTinkerers.enumBuilder("net.minecraft.client.InactivityFpsLimit", String.class, String.class)
             .addEnum("DISABLED", "disabled", "options.inactivityFpsLimit.disabled")
             .build();
     }
